3021c9f81d8140ca95a31e4aa5d360a19d75ccbb
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-18 11:06:17 +0100

    Initial commit.

---
+++ spec/SpecHelper.js
@@ -0,0 +1,18 @@
+beforeEach(function () {
+  jasmine.addMatchers({
+    // EXAMPLE
+    // toBePlaying: function () {
+    //   return {
+    //     compare: function (actual, expected) {
+    //       var player = actual;
+
+    //       return {
+    //         pass: player.currentlyPlayingSong === expected && player.isPlaying
+    //       }
+    //     }
+    //   };
+    // }
+
+    
+  });
+});
