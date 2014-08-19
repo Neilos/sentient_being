6149538780579dae1cb506de395e54034682f78d
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-19 17:54:34 +0100

    Minor function name changes for more readability.

--- src/sentient_being.js
+++ src/sentient_being.js
@@ -1,13 +1,13 @@
 var SentientBeing = function(blueprint) {
 
-  var getPronouns = function(pronoun, pronounCount) {
+  var personalPronounsFor = function(pronoun, pronounCount) {
     return Array.apply(null, new Array(pronounCount)).map(Object.prototype.valueOf,pronoun);
   };
 
-  var learnToComeALive = function(being, bringToLifeFunction) {
-    var pronounCount = bringToLifeFunction.length;
-    var thisPlusPronouns = [being].concat(getPronouns(being, pronounCount));
-    being.bringToLife = bringToLifeFunction.bind.apply(bringToLifeFunction, thisPlusPronouns);
+  var learnToComeALive = function(being, lifeFunc) {
+    var pronounCount = lifeFunc.length;
+    var thisPlusPronouns = [being].concat(personalPronounsFor(being, pronounCount));
+    being.bringToLife = lifeFunc.bind.apply(lifeFunc, thisPlusPronouns);
   };
 
   var isArray = function(possibleArray) {
