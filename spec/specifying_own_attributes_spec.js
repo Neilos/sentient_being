87bec8fc44a093bf56ff3152b659325b98ec4f23
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-19 13:37:00 +0100

    Attributes specified within ‘bringToLife’ function have access to those specified outside it (and with the personal pronoun syntax).

--- spec/specifying_own_attributes_spec.js
+++ spec/specifying_own_attributes_spec.js
@@ -12,6 +12,9 @@ describe("Specifying attributes by passing in an object,", function() {
             this.speed = this.speed * 2;
           };
           this.identity = this;
+          this.stateCatchphrase = function() {
+            return this.catchphrase;
+          }
         },
         catchphrase : "whaaaat?????",
         speedInWords : function() {
@@ -24,45 +27,53 @@ describe("Specifying attributes by passing in an object,", function() {
       });
     });
 
-    it("creates a new object with the attributes specified", function() {
-      expect(being.hair).toEqual("short");
-      expect(being.speed).toEqual(5);
-      being.run();
-      expect(being.speed).toEqual(10);
-      expect(being.identity).toBe(being);
-      expect(being.catchphrase).toEqual("whaaaat?????");
-    });
+    describe("creates a new object", function() {
 
-    it("attributes specified outside the 'bringToLife' function have access to attributes within the 'bringToLife' function", function() {
-      expect(being.speedInWords()).toEqual("slow");
-    })
-
-    it("attributes are NOT shared between multiple objects", function() {
-      being2 = new SentientBeing({
-        bringToLife : function() {
-          this.hair = "long";
-          this.speed = 7;
-          this.run = function() {
-            this.speed = this.speed * 3;
-          };
-          this.identity = this;
-        }
+      it("with the attributes specified", function() {
+        expect(being.hair).toEqual("short");
+        expect(being.speed).toEqual(5);
+        being.run();
+        expect(being.speed).toEqual(10);
+        expect(being.identity).toBe(being);
+        expect(being.catchphrase).toEqual("whaaaat?????");
       });
-      expect(being.identity).toEqual(being);
-      expect(being2.identity).toBe(being2);
 
-      expect(being.hair).toEqual("short");
-      expect(being2.hair).toEqual("long");
+      it("with attributes specified outside the 'bringToLife' function having access to attributes within the 'bringToLife' function", function() {
+        expect(being.speedInWords()).toEqual("slow");
+      })
 
-      expect(being.speed).toEqual(5);
-      expect(being2.speed).toEqual(7);
-      expect(being.speed).toEqual(5);
+      it("with attributes specified within the 'bringToLife' function having access to other attributes", function() {
+        expect(being.stateCatchphrase()).toEqual("whaaaat?????");
+      })
 
-      being.run();
-      being2.run();
+      it("with attributes NOT shared between multiple objects", function() {
+        being2 = new SentientBeing({
+          bringToLife : function() {
+            this.hair = "long";
+            this.speed = 7;
+            this.run = function() {
+              this.speed = this.speed * 3;
+            };
+            this.identity = this;
+          }
+        });
+        expect(being.identity).toEqual(being);
+        expect(being2.identity).toBe(being2);
 
-      expect(being.speed).toEqual(10);
-      expect(being2.speed).toEqual(21);
+        expect(being.hair).toEqual("short");
+        expect(being2.hair).toEqual("long");
+
+        expect(being.speed).toEqual(5);
+        expect(being2.speed).toEqual(7);
+        expect(being.speed).toEqual(5);
+
+        being.run();
+        being2.run();
+
+        expect(being.speed).toEqual(10);
+        expect(being2.speed).toEqual(21);
+      });
+
     });
 
   });
@@ -78,9 +89,13 @@ describe("Specifying attributes by passing in an object,", function() {
             my.speed = my.speed * 2;
           };
           my.identity = me;
+          I.stateCatchphrase = function() {
+            return my.catchphrase;
+          };
         },
         catchphrase : "whaaaat?????",
         speedInWords : function() {
+          // Will throw 'me' not defined exception
           if (me.speed > 5) {
             return "fast";
           } else {
@@ -90,45 +105,53 @@ describe("Specifying attributes by passing in an object,", function() {
       });
     });
 
-    it("a new object is created with the attributes specified", function() {
-      expect(being.hair).toEqual("short");
-      expect(being.speed).toEqual(5);
-      being.run();
-      expect(being.speed).toEqual(10);
-      expect(being.identity).toBe(being);
-      expect(being.catchphrase).toEqual("whaaaat?????");
-    });
+    describe("creates a new object", function() {
 
-    it("attributes specified outside the 'bringToLife' function do NOT have access to personal pronoun syntax", function() {
-      expect(being.speedInWords).toThrow();
-    })
-
-    it("attributes are NOT shared between multiple objects", function() {
-      being2 = new SentientBeing({
-        bringToLife : function(me, my, I) {
-          my.hair = "long";
-          my.speed = 7;
-          I.run = function() {
-            my.speed = my.speed * 3;
-          };
-          my.identity = me;
-        }
+      it("with the attributes specified", function() {
+        expect(being.hair).toEqual("short");
+        expect(being.speed).toEqual(5);
+        being.run();
+        expect(being.speed).toEqual(10);
+        expect(being.identity).toBe(being);
+        expect(being.catchphrase).toEqual("whaaaat?????");
       });
-      expect(being.identity).toEqual(being);
-      expect(being2.identity).toBe(being2);
 
-      expect(being.hair).toEqual("short");
-      expect(being2.hair).toEqual("long");
+      it("attributes specified outside the 'bringToLife' function do NOT have access to personal pronoun syntax", function() {
+        expect(being.speedInWords).toThrow();
+      })
 
-      expect(being.speed).toEqual(5);
-      expect(being2.speed).toEqual(7);
-      expect(being.speed).toEqual(5);
+      it("attributes specified within the 'bringToLife' function can access other attributes with personal pronoun syntax", function() {
+        expect(being.stateCatchphrase()).toEqual("whaaaat?????");
+      })
 
-      being.run();
-      being2.run();
+      it("attributes are NOT shared between multiple objects", function() {
+        being2 = new SentientBeing({
+          bringToLife : function(me, my, I) {
+            my.hair = "long";
+            my.speed = 7;
+            I.run = function() {
+              my.speed = my.speed * 3;
+            };
+            my.identity = me;
+          }
+        });
+        expect(being.identity).toEqual(being);
+        expect(being2.identity).toBe(being2);
 
-      expect(being.speed).toEqual(10);
-      expect(being2.speed).toEqual(21);
+        expect(being.hair).toEqual("short");
+        expect(being2.hair).toEqual("long");
+
+        expect(being.speed).toEqual(5);
+        expect(being2.speed).toEqual(7);
+        expect(being.speed).toEqual(5);
+
+        being.run();
+        being2.run();
+
+        expect(being.speed).toEqual(10);
+        expect(being2.speed).toEqual(21);
+      });
+
     });
 
   });
