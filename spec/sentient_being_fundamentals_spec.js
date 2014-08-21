b37aa3c7d43c0b6fa7a1783a48258b4cdd7586f2
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-21 17:33:30 +0100

    More existential stuff.

--- spec/sentient_being_fundamentals_spec.js
+++ spec/sentient_being_fundamentals_spec.js
@@ -6,6 +6,14 @@ describe("SentientBeing", function() {
     bob = new Human();
   });
 
+  it("is a function", function() {
+    expect(typeof SentientBeing).toBe('function');
+  });
+
+  it("was constructed by Function", function() {
+    expect(SentientBeing.constructor).toBe(Function);
+  });
+
   it("returns a constructor function", function() {
     expect(typeof Human).toBe("function");
     expect(typeof new Human()).toBe("object");
@@ -13,13 +21,18 @@ describe("SentientBeing", function() {
 
   describe("the returned constructor function", function() {
 
-    it("is God", function() {
-      expect(Human.name).toEqual("God");
+    it("is also a SentientBeing but is not THE SentientBeing", function() {
+      expect(Human.name).toEqual("SentientBeing");
+      expect(Human).not.toBe(SentientBeing);
+    });
+
+    it("was constructed by God", function() {
+      expect(Human.constructor.name).toEqual("God");
     });
 
   });
 
-  describe("object created with a SentientBeing constructor function", function() {
+  describe("object instance created with a SentientBeing constructor function", function() {
 
     it("is a 'typeof' object", function() {
       expect(typeof bob).toEqual("object");
@@ -35,25 +48,37 @@ describe("SentientBeing", function() {
 
   });
 
+  it("There is only one God", function() {
+    var god1 = Human.constructor;
+    var god2 = bob.constructor;
+    var god3 = god1.constructor;
+    var god4 = god1.prototype.constructor;
+    expect(god1 === god2 && god2 === god3 && god3 === god4).toBe(true);
+  });
+
   describe("God", function() {
     var god;
 
     beforeEach(function() {
       god = bob.constructor;
-    })
+    });
 
-    it("has a name", function() {
+    it("is a function", function() {
+      expect(typeof god).toBe('function');
+    });
+
+    it("has a name: God", function() {
       expect(god.name).toEqual("God");
     });
 
     it("was constructed by God", function() {
-      expect(god.constructor).toBe(god)
-      expect(god.constructor.name).toEqual("God")
+      expect(god.constructor).toBe(god);
+      expect(god.constructor.name).toEqual("God");
     })
 
     it("has a prototype that was constructed by God", function() {
-      expect(Human.prototype.constructor).toBe(god)
-      expect(Human.prototype.constructor.name).toEqual("God")
+      expect(god.prototype.constructor).toBe(god)
+      expect(god.prototype.constructor.name).toEqual("God")
     })
 
   });
--- src/sentient_being.js
+++ src/sentient_being.js
@@ -1,8 +1,10 @@
 var SentientBeing = function(blueprint) {
 
-  var NewBeing, DNA, Parent;
+  var SentientBeing, DNA, Parent, God;
 
-  NewBeing = function God() {}
+  SentientBeing = function SentientBeing() {
+
+  }
 
   if (blueprint && blueprint.parent) {
     Parent = blueprint.parent;
@@ -10,13 +12,54 @@ var SentientBeing = function(blueprint) {
     Parent = Object;
   }
 
+  God = function God(){ return SentientBeing;}
+  God.constructor = God;
+  God.prototype.constructor = God;
+
   DNA = function DNA() {};
   DNA.prototype = Parent.prototype;
-  NewBeing.prototype = new DNA();
-  NewBeing.parent = Parent.prototype;
-  NewBeing.prototype.constructor = NewBeing;
-  NewBeing.constructor = NewBeing;
+  SentientBeing.prototype = new DNA();
+  SentientBeing.parent = Parent.prototype;
+  SentientBeing.prototype.constructor = God;
+  SentientBeing.constructor = God;
 
-  return NewBeing;
+
+  return SentientBeing;
 }
 
+
+// var SentientBeing = function(Parent, props) {
+
+//   var SentientBeing;
+//   var DNA, i;
+
+//   // 1.
+//   // new constructor
+//   SentientBeing = function() {
+//     if (SentientBeing.parent && SentientBeing.parent.hasOwnProperty("__construct")) {
+//       SentientBeing.parent.__construct.apply(this, arguments);
+//     }
+//     if (SentientBeing.prototype.hasOwnProperty("__construct")) {
+//       SentientBeing.prototype.__construct.apply(this, arguments);
+//     }
+//   };
+
+//   // 2.
+//   // inherit
+//   Parent = Parent || Object;
+//   DNA = function() {};
+//   DNA.prototype = Parent.prototype;
+//   SentientBeing.prototype = new DNA();
+//   SentientBeing.parent = Parent.prototype;
+//   SentientBeing.prototype.constructor = SentientBeing;
+
+//   // 3.
+//   // add implementation methods
+//   for (i in props) {
+//     if (props.hasOwnProperty(i)) {
+//       SentientBeing.prototype[i] = props[i];
+//     }
+//   }
+
+//   return SentientBeing;
+// };
