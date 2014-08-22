9e36b463404e70dedf62881cecd963a519270fc7
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-22 11:48:29 +0100

    Test refactor.

--- spec/sentient_inheriting_from_parent_spec.js
+++ spec/sentient_inheriting_from_parent_spec.js
@@ -2,59 +2,71 @@ describe("SentientBeing inherits from parent;", function() {
   var Human, Ape, bob;
 
   beforeEach(function() {
-    Ape = {};
+    Ape = { dexterity : "poor" };
     Ape.prototype = {};
     Ape.prototype.communicate = function() { return "ook" };
     Human = SentientBeing({ parent : Ape });
     bob = new Human();
   });
 
-  describe("the returned constructor function", function() {
+  describe("an attribute of the parent (i.e. non-prototype attributes)", function() {
 
-    it("has a parent property that is set to the specified parent's prototype", function() {
-      expect(Human.parent).toBe(Ape.prototype);
-    })
+    describe("when the parent is a plain old javascript object", function() {
 
-    describe("an attribute inherited from the parent's prototype", function() {
-
-      it("can be called on object instances", function() {
-        expect(bob.communicate()).toEqual("ook");
-      });
-
-      it("is not a property of object instances", function() {
-        expect("communicate" in bob).toBe(true);
-        expect(bob.hasOwnProperty("communicate")).toBe(false);
-      });
-
-      it("is not property of the contructor function", function() {
-        expect(Human.hasOwnProperty("communicate")).toBe(false);
-      });
-
-      it("can be overridden in the constructor function's prototype", function() {
-        Human.prototype.communicate = function() { return "How do you do"; };
-        expect(bob.communicate()).toEqual("How do you do");
-        delete Human.prototype.communicate
-        expect(bob.communicate()).toEqual("ook");
+      it("attribute is NOT inherited", function() {
+        expect(bob.dexterity).toBe(undefined);
       });
 
     });
 
-    describe("an object instance", function() {
+  });
 
-      it("has a parent property that is set to the parent", function() {
-        expect(bob.parent).toBe(Ape);
-      });
+  describe("an attribute inherited from the parent's prototype", function() {
 
-      describe("when parent is a constructor function", function() {
+    it("can be called on object instances", function() {
+      expect(bob.communicate()).toEqual("ook");
+    });
 
-        it("is an instanceof <parent>", function() {
-          var Monkey = function() {};
-          var SpiderMonkey = SentientBeing({ parent : Monkey })
-          var fred = new SpiderMonkey();
-          console.log(fred.parent);
-          expect(fred instanceof fred.parent).toEqual(true);
-        });
+    it("is not a property of object instances", function() {
+      expect("communicate" in bob).toBe(true);
+      expect(bob.hasOwnProperty("communicate")).toBe(false);
+    });
 
+    it("is not property of the contructor function", function() {
+      expect(Human.hasOwnProperty("communicate")).toBe(false);
+    });
+
+    it("can be overridden in the constructor function's prototype", function() {
+      Human.prototype.communicate = function() { return "How do you do"; };
+      expect(bob.communicate()).toEqual("How do you do");
+      delete Human.prototype.communicate
+      expect(bob.communicate()).toEqual("ook");
+    });
+
+  });
+
+  describe("the returned constructor function", function() {
+
+    it("has a parent property that is set to the specified parent's prototype", function() {
+      expect(Human.parent).toBe(Ape.prototype);
+    });
+
+  });
+
+  describe("an object instance", function() {
+
+    it("has a parent property that is set to the parent", function() {
+      expect(bob.parent).toBe(Ape);
+    });
+
+    describe("when parent is a constructor function", function() {
+
+      it("is an instanceof <parent>", function() {
+        var Monkey = function() {};
+        var SpiderMonkey = SentientBeing({ parent : Monkey })
+        var fred = new SpiderMonkey();
+        console.log(fred.parent);
+        expect(fred instanceof fred.parent).toEqual(true);
       });
 
     });
