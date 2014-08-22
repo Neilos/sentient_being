784f2c408bd850206e29aa9d6293ca0ff96fbcda
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-22 11:26:21 +0100

    Refactor.

--- src/sentient_being.js
+++ src/sentient_being.js
@@ -1,5 +1,4 @@
 var SentientBeing = function(blueprint) {
-
   var SentientBeing, DNA, Parent, God;
 
   SentientBeing = function SentientBeing() {
@@ -17,60 +16,28 @@ var SentientBeing = function(blueprint) {
   God = function God(){ return SentientBeing;}
   God.constructor = God;
   God.prototype.constructor = God;
+  God.givePowerOfLifeTo = function(being) {
+    if (being.prototype.hasOwnProperty("bringToLife")) {
+      var lifeforce = being.prototype.bringToLife;
+      being.prototype.bringToLife = lifeforce.apply();
+    }
+  };
 
   DNA = function DNA() {};
   DNA.prototype = Parent.prototype;
   SentientBeing.prototype = new DNA();
   SentientBeing.parent = Parent.prototype;
-  SentientBeing.prototype.constructor = God;
-  SentientBeing.constructor = God;
 
   for (attribute in blueprint) {
     if (blueprint.hasOwnProperty(attribute)) {
-      if (attribute === "bringToLife") {
-        SentientBeing.prototype[attribute] = blueprint[attribute].call();
-      } else {
         SentientBeing.prototype[attribute] = blueprint[attribute];
-      }
     }
   }
 
+  SentientBeing.prototype.constructor = God;
+  SentientBeing.constructor = God;
+
+  God.givePowerOfLifeTo(SentientBeing);
+
   return SentientBeing;
 }
-
-
-// var SentientBeing = function(Parent, props) {
-
-//   var SentientBeing;
-//   var DNA, i;
-
-//   // 1.
-//   // new constructor
-//   SentientBeing = function() {
-//     if (SentientBeing.parent && SentientBeing.parent.hasOwnProperty("__construct")) {
-//       SentientBeing.parent.__construct.apply(this, arguments);
-//     }
-//     if (SentientBeing.prototype.hasOwnProperty("__construct")) {
-//       SentientBeing.prototype.__construct.apply(this, arguments);
-//     }
-//   };
-
-//   // 2.
-//   // inherit
-//   Parent = Parent || Object;
-//   DNA = function() {};
-//   DNA.prototype = Parent.prototype;
-//   SentientBeing.prototype = new DNA();
-//   SentientBeing.parent = Parent.prototype;
-//   SentientBeing.prototype.constructor = SentientBeing;
-
-//   // 3.
-//   // add implementation methods
-//   for (i in props) {
-//     if (props.hasOwnProperty(i)) {
-//       SentientBeing.prototype[i] = props[i];
-//     }
-//   }
-
-//   return SentientBeing;
-// };
