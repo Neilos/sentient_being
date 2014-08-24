195f0a38d4455595e9721ec2cf7b50cbca4564de
Neilos <nosniktatkinson@hotmail.co.uk>
2014-08-24 08:25:49 +0100

    Tidy up inheritance specs.

--- spec/inheriting_from_ancestor_species_spec.js
+++ spec/inheriting_from_ancestor_species_spec.js
@@ -1,4 +1,4 @@
-describe("Species inheriting from an ancestor species;", function() {
+describe("Species inheritance:", function() {
   var Human, Ape, bob;
 
   beforeEach(function() {
@@ -9,86 +9,18 @@ describe("Species inheriting from an ancestor species;", function() {
     bob = new Human();
   });
 
-  describe("an attribute of the ancestor species (i.e. non-prototype attributes)", function() {
+  describe("an attribute of the ancestor species' prototype", function() {
 
-    describe("when the ancestor species is a plain old javascript object", function() {
-
-      it("attribute is NOT inherited", function() {
-        expect(bob.dexterity).toBe(undefined);
-      });
-
-    });
-
-    describe("when ancestor species is another species (not just a POJO)", function() {
-      var Vulcan, Romulan, remus, spock
-
-      beforeEach(function() {
-
-        Vulcan = new Species({
-          bringToLife: function() {
-            this.haircut = "not an attribute"; // not an attribute of Vulcan
-            return function() {
-              this.ears = "pointy";
-              this.emotional = false;
-            };
-          },
-          homeworld : "Vulcan"
-        });
-
-        Romulan = Species({
-          ancestorSpecies : Vulcan,
-          bringToLife: function() {
-            return function() { this.emotional = true; };
-          }
-        });
-
-        remus = new Romulan();
-        spock = new Vulcan();
-      });
-
-      it("ancestor species' attributes are inherited as properties belonging to the child", function() {
-        expect(remus.ears).toEqual("pointy");
-        expect(remus.hasOwnProperty("ears")).toBe(true);
-      });
-
-      it("attributes declared on the ancestor species's prototype are accessible via an object instance's prototype", function() {
-        expect(remus.homeworld).toBe("Vulcan");
-        expect(remus.hasOwnProperty("homeworld")).toBe(false);
-        if (Object.prototype.hasOwnProperty("__proto__")) {
-          var remusDNA = remus.__proto__;
-          expect("homeworld" in remusDNA).toBe(true);
-
-          var ancestorSpeciesDNA = remusDNA.__proto__;
-          var spockDNA = spock.__proto__;
-          expect(ancestorSpeciesDNA).toBe(spockDNA);
-          expect(ancestorSpeciesDNA.hasOwnProperty("homeworld")).toBe(true);
-        }
-      });
-
-      it("ancestor species attributes do not overwrite child attributes", function() {
-        expect(remus.emotional).toBe(true);
-      });
-
-    });
-
-  });
-
-  describe("an attribute inherited from the ancestor species' prototype", function() {
-
-    it("can be called on object instances", function() {
+    it("is inherited (i.e. can be called on object instances)", function() {
       expect(bob.communicate()).toEqual("ook");
     });
 
-    it("is not a property of object instances", function() {
+    it("is not a property of a child species' instance", function() {
       expect("communicate" in bob).toBe(true);
       expect(bob.hasOwnProperty("communicate")).toBe(false);
     });
 
-    it("is not property of the contructor function", function() {
-      expect(Human.hasOwnProperty("communicate")).toBe(false);
-    });
-
-    it("can be overridden in the constructor function's prototype", function() {
+    it("can be overridden in the child Species", function() {
       Human.prototype.communicate = function() { return "How do you do"; };
       expect(bob.communicate()).toEqual("How do you do");
       delete Human.prototype.communicate
@@ -97,23 +29,48 @@ describe("Species inheriting from an ancestor species;", function() {
 
   });
 
-  describe("the returned constructor function", function() {
+  describe("a child species constructor function", function() {
 
-    it("has a ancestorSpecies property that is set to the specified ancestorSpecies's prototype", function() {
+    it("has a ancestorSpecies property set to the specified ancestorSpecies's prototype", function() {
       expect(Human.ancestorSpecies).toBe(Ape.prototype);
     });
 
   });
 
-  describe("an object instance", function() {
+  describe("an instance of a Child Species", function() {
 
-    it("has a ancestorSpecies property that is set to the ancestorSpecies", function() {
+    it("has a ancestorSpecies property set to the ancestorSpecies", function() {
       expect(bob.ancestorSpecies).toBe(Ape);
     });
 
-    describe("when ancestorSpecies is a constructor function", function() {
+  });
 
-      it("is an instanceof <ancestorSpecies>", function() {
+  describe("when ancestor species is an ordinary javascript object", function() {
+
+    describe("a direct attribute of the ancestor species (i.e. non-prototype attributes)", function() {
+
+      it("is NOT inherited", function() {
+        expect(bob.dexterity).toBe(undefined);
+      });
+
+    });
+
+    describe("a child species object instance", function() {
+
+      it("is NOT an 'instanceof' the ancestor species", function() {
+        function isInstanceOf() { bob instanceof Ape };
+        expect(isInstanceOf).toThrow();
+      });
+
+    });
+
+  });
+
+  describe("when ancestor species is a constructor function", function() {
+
+    describe("a child species object instance", function() {
+
+      it("is an 'instanceof' the ancestor species", function() {
         var Monkey = function() {};
         var SpiderMonkey = Species({ ancestorSpecies : Monkey })
         var fred = new SpiderMonkey();
@@ -124,6 +81,85 @@ describe("Species inheriting from an ancestor species;", function() {
 
   });
 
-});
 
+  describe("when ancestor species is another species (i.e. not just a POJO)", function() {
+    var Vulcan, Romulan, remus, spock
+
+    beforeEach(function() {
+
+      Vulcan = new Species({
+        bringToLife: function() {
+          this.haircut = "bad";
+          return function() {
+            this.ears = "pointy";
+            this.emotional = false;
+          };
+        },
+        homeworld : "Vulcan"
+      });
+
+      Romulan = Species({
+        ancestorSpecies : Vulcan,
+        bringToLife: function() {
+          return function() { this.emotional = true; };
+        }
+      });
+
+      spock = new Vulcan();
+      remus = new Romulan();
+    });
+
+    describe("a child species object instance", function() {
+
+      it("is an 'instanceof' the ancestor species", function() {
+        expect(remus instanceof Vulcan).toEqual(true);
+      });
+
+    });
+
+    describe("an attribute declared in the ancestor species' 'bringToLife' function", function() {
+
+      it("is an 'own' property of child species' object instances", function() {
+        expect(remus.ears).toEqual("pointy");
+        expect(remus.haircut).toEqual("bad");
+        expect(remus.hasOwnProperty("ears")).toBe(true);
+      });
+
+      it("can be overridden by child species without overwriting the ancestor attribute", function() {
+        expect(remus.emotional).toBe(true);
+        expect(spock.emotional).toBe(false);
+      });
+
+    });
+
+    describe("an attribute declared outside the ancestor species' 'bringToLife' function", function() {
+
+      if (Object.prototype.hasOwnProperty("__proto__")) {
+        it("is accessible by an object instance (via its prototype)", function() {
+          var remusDNA = remus.__proto__;
+          expect("homeworld" in remusDNA).toBe(true);
+
+          var ancestorSpeciesDNA = remusDNA.__proto__;
+          var spockDNA = spock.__proto__;
+          expect(ancestorSpeciesDNA).toBe(spockDNA);
+          expect(ancestorSpeciesDNA.hasOwnProperty("homeworld")).toBe(true);
+        });
+      }
+
+      it("is not an 'own' property of the child object (or its prototype)", function() {
+        expect(remus.homeworld).toBe("Vulcan");
+        expect(remus.hasOwnProperty("homeworld")).toBe(false);
+        remus.homeworld = "Earth";
+        remus.__proto__.homeworld = "Romulus";
+        expect(remus.homeworld).toBe("Earth");
+        expect(remus.__proto__.homeworld).toBe("Romulus");
+        expect(spock.homeworld).toBe("Vulcan");
+      });
+
+    });
+
+  });
+
+
+});
 
